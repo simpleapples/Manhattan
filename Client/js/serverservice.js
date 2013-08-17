d0b7082fdf3385063f8fcccd5cf09870c4af9a8e
luofei <luofeihit2010@gmail.com>
2013-08-17 19:28:24 +0800

    change readme

--- Client/js/serverservice.js
+++ Client/js/serverservice.js
@@ -2,21 +2,4 @@ var url = 'ws://106.187.96.242:8000',
     ws = new WebSocket(url),
     serverService = {
         connect : function (connHandler, msgHandler, closeHandler) {
-
-            ws.onopen = function () {
-                connHandler();
-            };
-
-            ws.onmessage = function (msg) {
-                msgHandler(msg.data);
-            }
-
-            ws.onclose = function () {
-                closeHandler();
-            }
-        },
-
-        send : function (msg) {
-            ws.send(msg);
-        }
-    };
+};
