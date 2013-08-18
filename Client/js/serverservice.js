98aca9f5638afea8eeb81ea583667401eb233a71
simpleapples <zangzhiya@gmail.com>
2013-08-18 21:26:59 +0800

    modify websocket send function

--- Client/js/serverservice.js
+++ Client/js/serverservice.js
@@ -1,5 +1,28 @@
 var url = 'ws://106.187.96.242:8000',
     ws = new WebSocket(url),
-    serverService = {
+	serverService = {
         connect : function (connHandler, msgHandler, closeHandler) {
-};
+			ws.onopen = function () {
+				connHandler();
+			};
+			
+			ws.onmessage = function (msg) {
+				msgHandler(msg.data);
+			}
+			
+			ws.onclose = function () {
+				closeHandler();
+			}
+		},
+		
+		send : function (type, uid, value) {
+			var msg;
+			if (value) {
+				msg = '{"type":"' + type + '", "uid":' + uid + ', "value":' + value + '}';
+			} else {
+				msg = '{"type":"' + type + '", "uid":' + uid + '}';
+			}
+			ws.send(msg);
+			console.log(msg);
+		}
+	};
