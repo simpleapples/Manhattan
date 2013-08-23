cbd5956d5046f32b97bd577106b75fa2a1a4d6df
zangzhiya <zangzhiya@gmail.com>
2013-08-23 14:55:10 +0800

    fix bug

--- Client/js/pptDraw.js
+++ Client/js/pptDraw.js
@@ -1,47 +1,47 @@
-var canvas = document.getElementById("ppt"),
-    context = canvas.getContext('2d'),
+var drawCanvas = document.getElementById("ppt"),
+    drawContext = drawCanvas.getContext('2d'),
     isDrawing = false,
     canvasRect = canvas.getBoundingClientRect();
 
-canvas.onmousedown = function (e) {
+drawCanvas.onmousedown = function (e) {
     isDrawing = true;
-    context.beginPath();
-    context.strokeStyle = "red"; 
-    context.lineWidth = 2;
-    context.moveTo(e.pageX - canvasRect.left, e.pageY - canvasRect.top);
+    drawContext.beginPath();
+    drawContext.strokeStyle = "red"; 
+    drawContext.lineWidth = 2;
+    drawContext.moveTo(e.pageX - canvasRect.left, e.pageY - canvasRect.top);
     serverService.send("DRMV", uid, '{"x":' + (e.pageX - canvasRect.left) + ', "y":' + (e.pageY - canvasRect.top) + '}');
     console.log("canvas down", e.pageX - canvasRect.left, e.pageY - canvasRect.top);
 }
 
-canvas.onmouseup = function (e) {
+drawCanvas.onmouseup = function (e) {
     isDrawing = false;
-    context.closePath();
+    drawContext.closePath();
     console.log("canvas up", e.pageX - canvasRect.left, e.pageY - canvasRect.top);			
 }
 
-canvas.onmousemove = function (e) {
+drawCanvas.onmousemove = function (e) {
     if (isDrawing) {
-        context.lineTo(e.pageX - canvasRect.left, e.pageY - canvasRect.top);
-        context.stroke();
+        drawContext.lineTo(e.pageX - canvasRect.left, e.pageY - canvasRect.top);
+        drawContext.stroke();
         serverService.send("DRLI", uid, '{"x":' + (e.pageX - canvasRect.left) + ', "y":' + (e.pageY - canvasRect.top) + '}');
         console.log("canvas move", e.pageX - canvasRect.left, e.pageY - canvasRect.top);
     }
 }
 
 function drawLineHandler(x, y) {
-    context.lineTo(x, y);
-    context.stroke();
+    drawContext.lineTo(x, y);
+    drawContext.stroke();
     console.log("draw line handler", x, y);
 }
 
 function drawMoveHandler(x, y) {
-    context.beginPath();
-    context.strokeStyle = "red"; 
-    context.lineWidth = 2;
-    context.moveTo(x, y); 
+    drawContext.beginPath();
+    drawContext.strokeStyle = "red"; 
+    drawContext.lineWidth = 2;
+    drawContext.moveTo(x, y); 
     console.log("draw move handler", x, y);
 }
 
 function drawCleanHandler() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    drawContext.clearRect(0, 0, canvas.width, canvas.height);
 }
--- Server/stream_server.js
+++ Server/stream_server.js
@@ -1,1 +1,2 @@
 var webRTC = require('webrtc.io').listen(8001);
+console.log("stream server run at port 8001")
